e89c16456d97df05ffb5d0b7af05504cd24cb135
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-09-25 17:36:26 -0500

    Update main.c

--- plugin_src/afr/source/main.c
+++ plugin_src/afr/source/main.c
@@ -36,15 +36,20 @@ void sys_proc_rw(const uintptr_t Address, const void *Data, const uint64_t Lengt
     }
     struct proc_rw process_rw_data;
     bzero(&process_rw_data, sizeof(process_rw_data));
+#if (__FINAL__) == 1
     process_rw_data.address = Address;
     process_rw_data.data = Data;
     process_rw_data.length = Length;
     process_rw_data.write_flags = 1;
-#if (__FINAL__) == 0
+    sys_sdk_proc_rw(&process_rw_data);
+#elif (__FINAL__) == 0
+    process_rw_data.address = Address;
+    process_rw_data.data = Data;
+    process_rw_data.length = Length;
     process_rw_data.write_flags = 0;
     debug_printf("address: 0x%lx\n", process_rw_data.address);
     debug_printf("before write:\n");
-    debug_printf("sys_sdk_proc_rw(&process_rw_data): 0x%08x\n", sys_sdk_proc_rw(&process_rw_data));
+    sys_sdk_proc_rw(&process_rw_data);
     debug_printf("hex_dump: ");
     hex_dump(process_rw_data.data, process_rw_data.length);
     debug_printf("after write:\n");
@@ -53,11 +58,9 @@ void sys_proc_rw(const uintptr_t Address, const void *Data, const uint64_t Lengt
     process_rw_data.data = Data;
     process_rw_data.length = Length;
     process_rw_data.write_flags = 1;
-    debug_printf("sys_sdk_proc_rw(&process_rw_data): 0x%08x\n", sys_sdk_proc_rw(&process_rw_data));
+    sys_sdk_proc_rw(&process_rw_data);
     debug_printf("hex_dump: ");
     hex_dump(process_rw_data.data, process_rw_data.length);
-#else
-    sys_sdk_proc_rw(&process_rw_data);
 #endif
 }
 
