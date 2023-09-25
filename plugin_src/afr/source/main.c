cb0b865a8d096d507f4d602437df8ca9cf6d23dd
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-09-25 15:00:08 -0500

    Update main.c

--- plugin_src/afr/source/main.c
+++ plugin_src/afr/source/main.c
@@ -162,10 +162,10 @@ s32 sceFiosFHOpen_hook(const void *arg1, int32_t *out_handle, const char *file_p
             *out_handle = fd;
             debug_printf("possible_path: %s\n", possible_path);
             debug_printf("*out_handle: %d\n", *out_handle);
-            return 0;
+            // return 0;
         }
     }
-    else
+    if (fd <= 0)
     {
         int32_t ret = sceFiosFHOpen(arg1, out_handle, file_path, arg4);
         debug_printf("sceFiosFHOpen: 0x%08x\n", ret);
@@ -192,10 +192,10 @@ s32 sceFiosFHOpenSync_hook(const void *arg1, int32_t *out_handle, const char *fi
             *out_handle = fd;
             debug_printf("possible_path: %s\n", possible_path);
             debug_printf("*out_handle: %d\n", *out_handle);
-            return 0;
+            // return 0;
         }
     }
-    else
+    if (fd <= 0)
     {
         int32_t ret = sceFiosFHOpenSync(arg1, out_handle, file_path, arg4);
         debug_printf("sceFiosFHOpenSync: 0x%08x\n", ret);
